98d9386a7121f1c71fd21e4d30faa7241cef5280
Vladimir Montchik <vlad.montchik@gmail.com>
2024-12-02 13:39:18 -0500

    initial impl for days 1 and 2

---
+++ DayOne/Program.cs
@@ -0,0 +1,36 @@
+﻿var firstIds = new List<int>();
+var secondIds = new List<int>();
+
+File.ReadAllLines(Path.GetFullPath(args[0]))
+    .Select(line => line.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+    .ToList()
+    .ForEach(splitLine =>
+    {
+        var first = int.Parse(splitLine[0]);
+        var second = int.Parse(splitLine[1]);
+        firstIds.Add(first);
+        secondIds.Add(second);
+    });
+
+firstIds.Sort();
+secondIds.Sort();
+
+var totalDistance = 0;
+var frequencyBucket = new int[100000];
+foreach (var (firstId, secondId) in firstIds.Zip(secondIds))
+{
+    var diff = Math.Abs(firstId - secondId);
+    totalDistance += diff;
+    
+    frequencyBucket[secondId]++;
+}
+
+Console.WriteLine("The total distance is {0}", totalDistance);
+
+var similarityScore = 0;
+foreach (var (firstId, _) in firstIds.Zip(secondIds))
+{
+    similarityScore += firstId * frequencyBucket[firstId];
+}
+
+Console.WriteLine("The similarity score is {0}", similarityScore);
---
+++ DayTwo/Program.cs
@@ -0,0 +1,107 @@
+﻿var lines = File
+    .ReadAllLines(Path.GetFullPath(args[0]))
+    .Select(x => x.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries))
+    .Select(x => x.Select(int.Parse).ToList())
+    .ToList();
+
+var totalSafe = lines.Sum(line => CheckSafelyIncreasing(line) || CheckSafelyDecreasing(line) ? 1 : 0);
+Console.WriteLine("There are {0} reports that are safe", totalSafe);
+
+var totalSafeWithRemoval = lines.Sum(line =>
+    CheckSafelyIncreasingWithRemoval(line) || CheckSafelyDecreasingWithRemoval(line) ? 1 : 0);
+Console.WriteLine("There are {0} reports that are safe with removal", totalSafeWithRemoval);
+
+return;
+
+static bool CheckSafelyIncreasingWithRemoval(IReadOnlyList<int> line)
+{
+    var safelyIncreasing = CheckSafelyIncreasing(line);
+
+    if (safelyIncreasing)
+        return true;
+
+    var safeInnerCount = 0;
+
+    for (var i = 0; i < line.Count; ++i)
+    {
+        var duplicate = new List<int>(line);
+
+        duplicate.RemoveAt(i);
+
+        var innerTest = CheckSafelyIncreasing(duplicate);
+
+        if (!innerTest)
+            continue;
+
+        safeInnerCount += 1;
+    }
+
+    return safeInnerCount > 0;
+}
+
+static bool CheckSafelyDecreasingWithRemoval(IReadOnlyList<int> line)
+{
+    var safelyDecreasing = CheckSafelyDecreasing(line);
+
+    if (safelyDecreasing)
+        return true;
+
+    var safeInnerCount = 0;
+
+    for (var i = 0; i < line.Count; ++i)
+    {
+        var duplicate = new List<int>(line);
+
+        duplicate.RemoveAt(i);
+
+        var innerTest = CheckSafelyDecreasing(duplicate);
+
+        if (!innerTest)
+            continue;
+
+        safeInnerCount += 1;
+    }
+
+    return safeInnerCount > 0;
+}
+
+static bool CheckSafelyIncreasing(IReadOnlyList<int> report)
+{
+    // -1 b/c windows of size 2 but need to look at last pair... it cancels out :(
+    for (var i = 0; i < report.Count - 1; i++)
+    {
+        var first = report[i];
+        var second = report[i + 1];
+
+        var diff = first - second;
+        var absDiff = Math.Abs(first - second);
+
+        if (diff < 0) // not increasing
+            return false;
+
+        if (absDiff is < 1 or > 3) // not safe
+            return false;
+    }
+
+    return true;
+}
+
+static bool CheckSafelyDecreasing(IReadOnlyList<int> report)
+{
+    for (var i = 0; i < report.Count - 1; i++)
+    {
+        var first = report[i];
+        var second = report[i + 1];
+
+        var diff = first - second;
+        var absDiff = Math.Abs(first - second);
+
+        if (diff > 0) // not decreasing
+            return false;
+
+        if (absDiff is < 1 or > 3) // not safe
+            return false;
+    }
+
+    return true;
+}
